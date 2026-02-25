4a86ec9ea437ce96429ee75c438b65bddd4e57b9
Tohar Kenin <tohar@example.com>
2026-02-25 18:09:20 +0200

    Backend - change category model items

--- backend/src/models/categories.ts
+++ backend/src/models/categories.ts
@@ -1,35 +1,35 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
 export interface Category extends Document {
+  id?: number;
   name: string;
-  slug: string;
-  description: string;
+  description?: string;
   createdAt: Date;
-  updatedAt: Date;
+  isActive: boolean;
   heder?: string;
   image?: string;
 }
 
 const CategorySchema: Schema = new Schema({
-  name: {
-    type: String,
-    required: true,
+  id: {
+    type: Number,
+    required: false,
   },
-  slug: {
+  name: {
     type: String,
     required: true,
   },
   description: {
     type: String,
-    required: true,
+    required: false,
   },
   createdAt: {
     type: Date,
     default: Date.now,
   },
-  updatedAt: {
-    type: Date,
-    default: Date.now,
+  isActive: {
+    type: Boolean,
+    default: true,
   },
   heder: {
     type: String,
