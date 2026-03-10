8b0d67effebb56c0ae15788a4308e5de35cc29e0
Tohar Kenin <tohar@example.com>
2026-03-10 13:51:52 +0200

    Backend - create s3 cofig and upload middleware

---
+++ backend/src/middleware/upload-image.ts
@@ -0,0 +1,18 @@
+import multer from 'multer';
+import multerS3 from 'multer-s3';
+import { s3 } from '../config/s3';
+
+const upload = multer({
+  storage: multerS3({
+    s3,
+    bucket: process.env.AWS_BUCKET_NAME || '',
+    acl: 'public-read',
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    key: (req, file, cb) => {
+      const uniqueName = `articles/${Date.now()}-${file.originalname}`;
+      cb(null, uniqueName);
+    },
+  }),
+});
+
+export default upload;
