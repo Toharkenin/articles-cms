b7b89ad0c4b17d7df4d5c14a0a2fbbee5434fcb3
Tohar Kenin <tohar@example.com>
2026-03-11 13:07:43 +0200

    Frontend - fetch create admin

--- frontend/services/auth.ts
+++ frontend/services/auth.ts
@@ -26,3 +26,26 @@ export const adminLogout = async () => {
     throw error;
   }
 };
+
+export const createAdmin = async (
+  firstName: string,
+  lastName: string,
+  email: string,
+  password: string,
+  phoneNumber: string,
+  role: string
+) => {
+  try {
+    const response = await axiosInstance.post('/auth/create-admin', {
+      firstName,
+      lastName,
+      email,
+      password,
+      phoneNumber,
+      role,
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
